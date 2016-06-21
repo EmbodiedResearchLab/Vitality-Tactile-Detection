f85dc6de30f917ec9151055d13a23026f03c2658
brendancullen1 <brendan_cullen@brown.edu>
2016-06-21 14:12:58 -0400

    uncommented practice/PEST

--- ChannelBeeperSingle/runSubScript.m
+++ ChannelBeeperSingle/runSubScript.m
@@ -2,7 +2,7 @@
 % Script runs after you deciding one stimulator will be used.
 
 %% 2) Display Instructions + Training Block 1 (expected time: 4 minutes)
-%{
+%
 fprintf('==========\nDisplay Instructions and Training Block\n==========\n')
 not_understand_task = true;
 
@@ -75,7 +75,7 @@ end
 %}
 
 %% 4) Tactile Detection Protocol
-detection_threshold = .38;
+%detection_threshold = .38;
 % Call Dynamic Thresholding
 [output_array, subject_quit_tactile_detection, new_threshold, tactile_task] = Channel_Dynamic_Thresholding(windowPtr, detection_threshold);
 
