8bb66f5c865cf7747b2afc015f085042619ee700
brendancullen1 <brendan_cullen@brown.edu>
2016-07-20 17:30:01 -0400

    Fixed Trigger in Channel_Dynamic_Thresholding_Doubkle

--- ChannelBeeperDouble/runSubScript.m
+++ ChannelBeeperDouble/runSubScript.m
@@ -2,7 +2,7 @@
 % Runs subscript after deciding two stimulators will be used.
 
 %% 2) Display Instructions + Training Block (expected time: 4 minutes)
-%
+%{
 fprintf('==========\nDisplay Instructions and Training Block\n==========\n')
 not_understand_task = true;
 
@@ -35,7 +35,7 @@ end
 
 %}
 %% 3) PEST Convergence Procedure
-%
+%{
 display_instructions_NEW(windowPtr,2)
 % Pest Directed to one hand and then the other hand.
 fprintf('==========\nPEST Convergence Procedure on LEFT Hand\n==========\n')
@@ -87,8 +87,8 @@ while not_understand_task
 end
 %}
 %% 4) Tactile Detection Protocol
-% left_threshold = .38;
-% right_threshold = .38;
+left_threshold = .38;
+right_threshold = .38;
 display_instructions_NEW(windowPtr,5)
 [output_array, subject_quit, new_left_threshold, new_right_threshold, left, right] = Channel_Dynamic_Thresholding_Double(windowPtr, left_threshold, right_threshold);
 
